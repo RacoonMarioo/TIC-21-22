bd9f8a63e57266069269ff23336a9899372a7e2a
RacoonMarioo <100280968+RacoonMarioo@users.noreply.github.com>
2022-04-21 08:39:03 +0200

    Add files via upload

---
+++ personas.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package ProyectoManolo;
+
+/**
+ * @author Usuario
+ *
+ */
+public class personas {
+	string
+
+	/**
+	 * 
+	 */
+	public personas() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
